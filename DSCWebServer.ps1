0d42ce069a6190ccf74cc3587ea9279207f2f840
roushankhan <roushan.khan@outlook.com>
2016-10-19 16:52:39 +0530

    Add files via upload

---
+++ DSCWebServer.ps1
@@ -0,0 +1,39 @@
+﻿#********Roushan Khan , Date: 10/10/2016 ************
+<#This is a DSC Configuration for a Web server with copying website files from a source/Repo to a destination/web server location.
+All need to be done is run the below commands.
+Start-DscConfiguration -Path 'C:\Users\Roushan_Khan\Desktop\' -ComputerName $ComputerName -Force -Wait
+You can use a Pull Server for Enterprise or a Push server less recommended with HTTPS type
+PullServer ref: https://msdn.microsoft.com/en-us/powershell/dsc/pullserver#>
+
+$SourceFile = 'C:\Temp\WebCodes' # change this as its just an example
+$DestinationFile = 'C:\Websites\'  # change this as its just an example
+$ComputerName = $env:COMPUTERNAME
+Configuration WebServerNode
+{
+    Node $ComputerName
+    {
+        WindowsFeature IISRoleDSC
+        {
+            Name = 'Web-Server'
+            Ensure = 'Present'
+            IncludeAllSubFeature = $true
+            
+            
+        }
+        File CopyWesiteCode      
+        {          
+            Ensure = 'Present'         
+            Type = 'Directory'       
+            SourcePath = $SourceFile      
+            DestinationPath = $DestinationFile
+            Recurse = $true
+            Force = $true
+        } 
+    }
+}
+
+WebServerNode -OutputPath 'C:\Users\VANAdmin01\Desktop\'
+
+#then start the desired state configuration on a target machine as below.
+
+Start-DscConfiguration -Path 'MOF File path' -Wait -Force -Verbose -ComputerName -Credential
