03f99f1e4f63d35aea5d285f84144c96cc7881c3
roushankhan <roushan.khan@outlook.com>
2016-10-19 16:54:09 +0530

    Update DSCWebServer.ps1

--- DSCWebServer.ps1
+++ DSCWebServer.ps1
@@ -1,7 +1,7 @@
 ﻿#********Roushan Khan , Date: 10/10/2016 ************
 <#This is a DSC Configuration for a Web server with copying website files from a source/Repo to a destination/web server location.
 All need to be done is run the below commands.
-Start-DscConfiguration -Path 'C:\Users\Roushan_Khan\Desktop\' -ComputerName $ComputerName -Force -Wait
+Start-DscConfiguration -Path 'C:\Users\UserName\Desktop\' -ComputerName $ComputerName -Force -Wait
 You can use a Pull Server for Enterprise or a Push server less recommended with HTTPS type
 PullServer ref: https://msdn.microsoft.com/en-us/powershell/dsc/pullserver#>
 
@@ -32,7 +32,7 @@ Configuration WebServerNode
     }
 }
 
-WebServerNode -OutputPath 'C:\Users\VANAdmin01\Desktop\'
+WebServerNode -OutputPath 'Output Path'
 
 #then start the desired state configuration on a target machine as below.
 
